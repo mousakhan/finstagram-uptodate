b7f6f7c03967691196ab10388e66f959b785c8d9
Mousa Khan <mousakhan95@gmail.com>
2022-02-05 14:33:01 +0000

    w4d2

--- app/actions.rb
+++ app/actions.rb
@@ -1,12 +1,24 @@
-def humanized_time_ago(minute_num)
-  if minute_num >= 60
-    "#{minute_num / 60} hours ago"
-  else
-    "#{minute_num} minutes ago"
-  end
-end
-
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
+end
+
+get '/signup' do     # if a user navigates to the path "/signup",
+  @user = User.new   # setup empty @user object
+  erb(:signup)       # render "app/views/signup.erb"
+end
+
+post '/signup' do
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+    "User #{username} saved!"
+  else
+    erb(:signup)
+  end
 end
